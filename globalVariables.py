9cd569afdb6dfce8c08c6e43e8f21c395bac50bf
KaiHansen22 <166645695+KaiHansen22@users.noreply.github.com>
2024-05-02 12:17:06 -0600

    Update globalVariables.py
    added more global variables

--- globalVariables.py
+++ globalVariables.py
@@ -1,15 +1,25 @@
 frame = 0
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-
+decision = 1
+supplies = 10
+crew = 3
+broken_solar = False
 
 def init():
     global frame
     frame = 0
 
     global SCREEN_WIDTH
+    global decision
+    decision = 0
+    global supplies
+    supplies = 10
+    global crew
+    crew = 3
+    global broken_solar
+    broken_solar = False
     SCREEN_WIDTH = 800
     global SCREEN_HEIGHT
     SCREEN_HEIGHT = 800
     global screen
-    
