58fd1d1625daf947f26b093ed49aa72e5f1d12a8
KaiHansen22 <166645695+KaiHansen22@users.noreply.github.com>
2024-05-02 12:18:04 -0600

    Create consoleKAI.py

---
+++ consoleKAI.py
@@ -0,0 +1,80 @@
+import pygame
+import sys
+
+pygame.init()
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode([600, 500])
+base_font = pygame.font.Font(None, 32)
+user_text = ''
+input_rect = pygame.Rect(200, 200, 140, 32)
+color_active = pygame.Color('lightskyblue3')
+color_passive = pygame.Color('chartreuse4')
+color = color_passive
+active = False
+
+def init():
+    pass
+
+def draw():
+    global user_text, active, color
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if input_rect.collidepoint(event.pos):
+                active = True
+            else:
+                active = False
+
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_RETURN:
+                    active = False
+                    user_text = user_text.strip()
+                    output(user_text)
+                    user_text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    user_text = user_text[:-1]
+                else:
+                    user_text += event.unicode
+
+    screen.fill((255, 255, 255))
+
+    if active:
+        color = color_active
+    else:
+        color = color_passive
+
+    pygame.draw.rect(screen, color, input_rect)
+
+    text_surface = base_font.render(user_text, True, (255, 255, 255))
+    screen.blit(text_surface, (input_rect.x+5, input_rect.y+5))
+
+    input_rect.w = max(100, text_surface.get_width()+10)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+def output(text):
+    text_surface = base_font.render(text, True, (0, 0, 0))
+    screen.blit(text_surface, (input_rect.x, input_rect.y + 40))
+    pygame.display.flip()
+
+def key_pressed(key):
+    global user_text, active
+    if active:
+        if key == pygame.K_RETURN:
+            active = False
+            user_text = user_text.strip()
+            output(user_text)
+            user_text = ''
+        elif key == pygame.K_BACKSPACE:
+            user_text = user_text[:-1]
+        else:
+            user_text += pygame.key.name(key)
+
