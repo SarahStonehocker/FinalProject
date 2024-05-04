599c780714584368f593185101748714d8824737
KaiHansen22 <166645695+KaiHansen22@users.noreply.github.com>
2024-05-04 12:24:36 -0600

    Update globalVariables.py
    final globals

--- globalVariables.py
+++ globalVariables.py
@@ -1,10 +1,12 @@
 frame = 0
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-decision = 0
+decision = 1
 supplies = 10
+user_input = ""
 crew = 3
 broken_solar = False
+victory = False
 
 def init():
     global frame
@@ -14,7 +16,9 @@ def init():
     global decision
     decision = 0
     global supplies
+    global victory
     supplies = 10
+    global user_input
     global crew
     crew = 3
     global broken_solar
